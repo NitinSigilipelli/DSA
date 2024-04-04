a70477ba2f7090a353fc00e2d47d2cc437c32160
Nitin <140246876+NitinSigilipelli@users.noreply.github.com>
2024-04-04 20:42:07 +0530

    Create SortColors.java
    leetcode 75 optimal solution

---
+++ SortColors.java
@@ -0,0 +1,28 @@
+class Solution {
+    void swap(int[] nums,int i ,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0 ; 
+        while(mid<= high){
+            System.out.println("Low:"+ low +"Mid:"+mid+ "High :"+ high+" "+Arrays.toString(nums));
+            
+            if(nums[mid] == 0){
+                swap(nums,mid,low);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] ==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+}
